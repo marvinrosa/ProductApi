c35677674de82d3c7bbc76e600a55d4d516ead16
Marvin Rosa <marvin.fortin@outlook.com>
2022-05-15 07:05:24 -0600

    Configurando la fuente de datos

---
+++ src/test/java/dev/marvinrosa/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marvinrosa.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
